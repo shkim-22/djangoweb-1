d06f23d6d6dbe1f1465d887a4f3bd2e83b4cc9e1
DESKTOP-07K8134\admin <ahn1492@gmail.com>
2024-09-23 10:26:38 +0900

    첫번째 커밋

---
+++ mysite/main/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import redirect, render
+from .models import *
+
+# Create your views here.
+def index(request):
+    #templates폴더 안에있는 폴더 및 html파일의 경로를 설정
+    return render(request,'main/index.html')
+
+def notice(request):
+    noticelist = Notice.objects.all()
+    return render(request,'main/notice_list.html', {'noticeList':noticelist})
+
+def notice_view(request,pk):
+    notice = Notice.objects.get(pk=pk)
+    return render(request, 'main/notice_view.html', {'notice':notice})
+
+def notice_add(request):
+    if request.method == 'POST':
+        new_notice = Notice.objects.create(
+            title = request.POST['title'],
+            contents = request.POST['contents'],
+            views = 0,
+        )
+        return redirect('/notice/')
+    return render(request, 'main/notice_add.html')
+
+def notice_remove(request, pk):
+    notice = Notice.objects.get(pk=pk)
+    if request.method == 'POST':
+        notice.delete()
+    return redirect('/notice/')
+    #return render(request, 'main/remove_post.html')
+
+def program(request):
+    programlist = Program.objects.all()
+    return render(request, 'main/program.html', {'programList':programlist})
---
+++ mysite/py.py
@@ -0,0 +1,8 @@
+sum = 0
+a =1
+while a < 101:
+    if a % 3 == 0:
+        sum = sum + a
+    a = a + 1
+
+print(sum)
