4350f8976e278eef182bf9fcd4488725973842ed
ahn1492 <ahn1492@gmail.com>
2024-11-03 02:15:22 +0900

    Create django2.sh

---
+++ mysite/django2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git clone https://github.com/ahn1492/djangoweb.git
+
+python3 /djangoweb/mysite/manage.py makemigrations
+sleep 5s
+
+python3 /djangoweb/mysite/manage.py migrate
+sleep 10s
+
+python3 /djangoweb/mysite/manage.py runserver 0.0.0.0:8000
